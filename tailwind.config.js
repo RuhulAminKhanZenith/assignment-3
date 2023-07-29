ab71273ae88786bac8c596741ebc5aa69e8cb11d
RuhulAminKhanZenith <ruhul.amin.khan.zenith@gmail.com>
2023-07-29 20:48:46 +0600

    edited section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/*module.exports = {
     //...
     plugins: [require("daisyui")],
   }
